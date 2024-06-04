947158887862385ae8d40e8c1894bb5161d1cb53
TaufiiquRahman <taufiiqurahman@gmail.com>
2024-06-05 00:38:25 +0700

    Update util.py

--- util.py
+++ util.py
@@ -4,16 +4,8 @@ from PIL import ImageOps, Image
 import numpy as np
 import tensorflow as tf
 
+# Function to set background image for Streamlit app
 def set_background(image_file):
-    """
-    This function sets the background of a Streamlit app to an image specified by the given image file.
-
-    Parameters:
-        image_file (str): The path to the image file to be used as the background.
-
-    Returns:
-        None
-    """
     with open(image_file, "rb") as f:
         img_data = f.read()
     b64_encoded = base64.b64encode(img_data).decode()
@@ -27,19 +19,8 @@ def set_background(image_file):
     """
     st.markdown(style, unsafe_allow_html=True)
 
-def classify(image, model, class_names):
-    """
-    This function takes an image, a model, and a list of class names and returns the predicted class and confidence
-    score of the image.
-
-    Parameters:
-        image (PIL.Image.Image): An image to be classified.
-        model (tensorflow.keras.Model): A trained machine learning model for image classification.
-        class_names (list): A list of class names corresponding to the classes that the model can predict.
-
-    Returns:
-        A tuple of the predicted class name and the confidence score for that prediction.
-    """
+# Function to classify images using a trained model
+def classify_image(image, model, class_names):
     # Check if the image is grayscale
     if image.mode != 'L':
         image = ImageOps.grayscale(image)
@@ -59,7 +40,7 @@ def classify(image, model, class_names):
 
     # Determine the predicted class and confidence score
     index = np.argmax(prediction)
-    class_name = class_names[index]
+    predicted_class = class_names[index]
     confidence_score = prediction[0][index]
 
-    return class_name, confidence_score
+    return predicted_class, confidence_score
