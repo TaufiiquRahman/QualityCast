625485f247eb29703beaa0e20470bc33e57a22eb
TaufiiquRahman <taufiiqurahman@gmail.com>
2024-06-05 01:35:22 +0700

    Update 7_🏠_Home.py

--- 7_🏠_Home.py
+++ 7_🏠_Home.py
@@ -137,7 +137,4 @@ if st.button('Show Predictions on Test Images'):
     test_set = test_set.batch(batch_size)
 
     # Visualize predictions on test set
-    visualize_predictions(model, test_set, class_map, img_size, batch_size)
-
-    # Visualize misclassified images on test set
-    visualize_misclassified(model, test_set, class_map, img_size, batch_size)
+    visualize_predictions(model, test_set, class_map
