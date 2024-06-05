fc4418739b8d5726ac36af15cf316aeae151301a
TaufiiquRahman <taufiiqurahman@gmail.com>
2024-06-05 12:39:42 +0700

    change color_🏠_Home.py

--- 7_🏠_Home.py
+++ 7_🏠_Home.py
@@ -88,7 +88,7 @@ if file is not None:
         fig, ax = plt.subplots()
         sizes = [score for class_name, score in top_classes]
         labels = [f'{class_name} ({score*100:.1f}%)' for class_name, score in top_classes]
-        colors = ['blue' if class_name == "Perfect" else 'red' for class_name, _ in top_classes]
+        colors = ['white' if class_name == "Perfect" else 'red' for class_name, _ in top_classes]
         ax.pie(sizes, labels=labels, colors=colors, startangle=90, counterclock=False, wedgeprops={'width': 0.3, 'edgecolor': 'w'})
         ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
         st.pyplot(fig)
