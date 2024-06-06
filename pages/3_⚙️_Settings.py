1df317955a5e4f97e5d80198f9f1bc3b09567fe7
TaufiiquRahman <taufiiqurahman@gmail.com>
2024-06-06 19:11:57 +0700

    Update 3_⚙️_Settings.py

--- pages/3_⚙️_Settings.py
+++ pages/3_⚙️_Settings.py
@@ -1,8 +1,9 @@
 import streamlit as st
+import auth_functions
 
 
 
-
-
-st.title("Settings")
-
+# Delete Account
+st.header('Delete account:')
+password = st.text_input(label='Confirm your password',type='password')
+st.button(label='Delete Account',on_click=auth_functions.delete_account,args=[password],type='primary') 
