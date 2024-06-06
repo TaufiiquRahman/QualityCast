b7b94f983c32896220294c3820f1ed6b310dc514
TaufiiquRahman <taufiiqurahman@gmail.com>
2024-06-06 18:54:11 +0700

    add login

--- 7_🏠_Home.py
+++ 7_🏠_Home.py
@@ -7,6 +7,7 @@ import pandas as pd
 from util import classify, set_background
 from datetime import datetime
 import os
+import auth_functions
 
 # Set background
 set_background('./bgrd/bg.jpg')
@@ -44,99 +45,129 @@ st.markdown(
     """, unsafe_allow_html=True
 )
 
-# Set title
-st.markdown('<div class="title-box">Casting Quality Control</div>', unsafe_allow_html=True)
+# -------------------------------------------------------------------------------------------------
+# Main
+# -------------------------------------------------------------------------------------------------
+if 'user_info' not in st.session_state:
+    col1, col2, col3 = st.columns([1, 2, 1])
 
-# Set header
-st.markdown('<div class="header-box">Please upload a Casting Product Image</div>', unsafe_allow_html=True)
+    # Authentication form layout
+    do_you_have_an_account = col2.selectbox(label='Do you have an account?', options=('Yes', 'No', 'I forgot my password'))
+    auth_form = col2.form(key='Authentication form', clear_on_submit=False)
+    email = auth_form.text_input(label='Email')
+    password = auth_form.text_input(label='Password', type='password') if do_you_have_an_account in {'Yes', 'No'} else auth_form.empty()
+    auth_notification = col2.empty()
 
-# Upload file
-file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
+    # Sign In
+    if do_you_have_an_account == 'Yes' and auth_form.form_submit_button(label='Sign In', use_container_width=True, type='primary'):
+        with auth_notification, st.spinner('Signing in'):
+            auth_functions.sign_in(email, password)
 
-# Load classifier
-model = load_model('./model.h5')
+    # Create Account
+    elif do_you_have_an_account == 'No' and auth_form.form_submit_button(label='Create Account', use_container_width=True, type='primary'):
+        with auth_notification, st.spinner('Creating account'):
+            auth_functions.create_account(email, password)
 
-# Load class names
-with open('./model/labels.txt', 'r') as f:
-    class_names = [line.strip().split(' ')[1] for line in f.readlines()]
+    # Password Reset
+    elif do_you_have_an_account == 'I forgot my password' and auth_form.form_submit_button(label='Send Password Reset Email', use_container_width=True, type='primary'):
+        with auth_notification, st.spinner('Sending password reset link'):
+            auth_functions.reset_password(email)
 
-# Print class names for debugging
-print("Loaded class names:", class_names)
+    # Authentication success and warning messages
+    if 'auth_success' in st.session_state:
+        auth_notification.success(st.session_state.auth_success)
+        del st.session_state.auth_success
+    elif 'auth_warning' in st.session_state:
+        auth_notification.warning(st.session_state.auth_warning)
+        del st.session_state.auth_warning
 
-# Display image and classification results
-if file is not None:
-    # Create two columns for layout
-    col1, col2 = st.columns(2)
-    
-    # Column 1: Image and file name
-    with col1:
-        image = Image.open(file).convert('RGB')
-        st.image(image, use_column_width=True)
-        st.markdown(f'<div class="filename-box">Uploaded file: {file.name}</div>', unsafe_allow_html=True)
-    
-    # Column 2: Classification result and donut chart
-    with col2:
-        # Classify image
-        top_classes = classify(image, model, class_names, top_n=1)
-        
-        # Print top_classes for debugging
-        print("Top classes:", top_classes)
-        
-        # Display the top class and its confidence score
-        top_class_name, top_conf_score = top_classes[0]
-        top_conf_percentage = top_conf_score * 100
+else:
+    # Set title
+    st.markdown('<div class="title-box">Casting Quality Control</div>', unsafe_allow_html=True)
 
-        # Get the index of the second class (other than the top class)
-        second_class_index = 1 if top_class_name == class_names[0] else 0
+    # Set header
+    st.markdown('<div class="header-box">Please upload a Casting Product Image</div>', unsafe_allow_html=True)
 
-        # Get the name and score of the second class
-        second_class_name = class_names[second_class_index]
-        second_conf_score = 1 - top_conf_score
-        second_conf_percentage = 100 - top_conf_percentage
+    # Upload file
+    file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
 
-        # Display the box with scores of both classes
-        st.markdown(f"""
-        <div class="box">
-            <h2 style="color: white; text-align: center;">Result</h2>
-            <h3 style="color: white;">{top_class_name} - {top_conf_percentage:.1f}%</h3>
-            <h3 style="color: white;">{second_class_name} - {second_conf_percentage:.1f}%</h3>
-        </div>
-        """, unsafe_allow_html=True)
-        
-        # Create a donut chart
-        fig, ax = plt.subplots()    
-        sizes = [top_conf_score, second_conf_score]
-        labels = [f'{class_name} ({conf_percentage:.1f}%)' for class_name, conf_percentage in zip([top_class_name, second_class_name], [top_conf_percentage, second_conf_percentage])]
-        colors = ['#ff9999', '#66b3ff']
-        ax.pie(sizes, labels=labels, colors=colors, startangle=90, counterclock=False, wedgeprops={'width': 0.3, 'edgecolor': 'w'})
-        ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-        
-        # Display the donut chart
-        st.pyplot(fig)
-        
-        # Save the result to history
-        log = pd.DataFrame([{
-            "filename": file.name,
-            "class_name": top_class_name,
-            "confidence_score": f"{top_conf_percentage:.1f}%",
-            "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        }])
-        
-        # Print log for debugging
-        print("Log entry:", log)
-        
-        # Load existing history if available
-        history_path = os.path.join(os.path.dirname(__file__), 'pages/history.csv')
-        try:
-            history = pd.read_csv(history_path)
-        except FileNotFoundError:
-            history = pd.DataFrame(columns=["filename", "class_name", "confidence_score", "timestamp"])
-        
-        # Append new log using pd.concat
-        history = pd.concat([history, log], ignore_index=True)
-        
-        # Print updated history for debugging
-        print("Updated history:", history)
-        
-        # Save updated history
-        history.to_csv(history_path, index=False)
+    # Load classifier
+    model = load_model('./model.h5')
+
+    # Load class names
+    with open('./model/labels.txt', 'r') as f:
+        class_names = [line.strip().split(' ')[1] for line in f.readlines()]
+
+    # Display image and classification results
+    if file is not None:
+        # Create two columns for layout
+        col1, col2 = st.columns(2)
+
+        # Column 1: Image and file name
+        with col1:
+            image = Image.open(file).convert('RGB')
+            st.image(image, use_column_width=True)
+            st.markdown(f'<div class="filename-box">Uploaded file: {file.name}</div>', unsafe_allow_html=True)
+
+        # Column 2: Classification result and donut chart
+        with col2:
+            # Classify image
+            top_classes = classify(image, model, class_names, top_n=1)
+
+            # Display the top class and its confidence score
+            top_class_name, top_conf_score = top_classes[0]
+            top_conf_percentage = top_conf_score * 100
+
+            # Get the index of the second class (other than the top class)
+            second_class_index = 1 if top_class_name == class_names[0] else 0
+
+            # Get the name and score of the second class
+            second_class_name = class_names[second_class_index]
+            second_conf_score = 1 - top_conf_score
+            second_conf_percentage = 100 - top_conf_percentage
+
+            # Display the box with scores of both classes
+            st.markdown(f"""
+            <div class="box">
+                <h2 style="color: white; text-align: center;">Result</h2>
+                <h3 style="color: white;">{top_class_name} - {top_conf_percentage:.1f}%</h3>
+                <h3 style="color: white;">{second_class_name} - {second_conf_percentage:.1f}%</h3>
+            </div>
+            """, unsafe_allow_html=True)
+
+            # Create a donut chart
+            fig, ax = plt.subplots()
+            sizes = [top_conf_score, second_conf_score]
+            labels = [f'{class_name} ({conf_percentage:.1f}%)' for class_name, conf_percentage in zip([top_class_name, second_class_name], [top_conf_percentage, second_conf_percentage])]
+            colors = ['#ff9999', '#66b3ff']
+            ax.pie(sizes, labels=labels, colors=colors, startangle=90, counterclock=False, wedgeprops={'width': 0.3, 'edgecolor': 'w'})
+            ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+            # Display the donut chart
+            st.pyplot(fig)
+
+            # Save the result to history
+            log = pd.DataFrame([{
+                "filename": file.name,
+                "class_name": top_class_name,
+                "confidence_score": f"{top_conf_percentage:.1f}%",
+                "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            }])
+
+            # Load existing history if available
+            history_path = os.path.join(os.path.dirname(__file__), 'pages/history.csv')
+            try:
+                history = pd.read_csv(history_path)
+            except FileNotFoundError:
+                history = pd.DataFrame(columns=["filename", "class_name", "confidence_score", "timestamp"])
+
+            # Append new log using pd.concat
+            history = pd.concat([history, log], ignore_index=True)
+
+            # Save updated history
+            history.to_csv(history_path, index=False)
+
+    # Sign out
+    if st.sidebar.button(label='Sign Out', on_click=auth_functions.sign_out, type='primary'):
+        pass  #
+
